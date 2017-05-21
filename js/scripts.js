a5aa95e7b0ae15b6e1b871a171e74ddb06b288f7
mjtheriot4 <mther16@lsu.edu>
2017-05-21 12:37:07 -0500

    added stand-in mobile menu

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+//Mobile Menu
+  $("#navBug").click(function(){
+      $('nav').slideToggle('fast');
+      if ($(this).attr("src") == "images/menuOpen.png") {
+        $(this).attr("src", "images/menuClosed.png");
+      }
+      else {$(this).attr("src", "images/menuOpen.png")}
+  });
+  $("body > div.layout").click(function(){
+      $('nav').slideUp('fast');
+      $('#navBug').attr("src", "images/menuClosed.png")
+  });
+});
